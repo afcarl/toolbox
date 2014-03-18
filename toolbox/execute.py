81f70f472636b35ad123fb63db871ef154bac870
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-18 14:47:01 +0100

    added quiet flag to execute

--- toolbox/execute.py
+++ toolbox/execute.py
@@ -4,15 +4,16 @@ import subprocess
 c_end     = '\033[0m'
 c_cyan    = '\033[0;36m'
 
-def execute(cmd, verbose=True):
-    print(cmd)
+def execute(cmd, verbose=True, quiet=False):
+    if not quiet:
+        print(cmd)
     try:
         proc = subprocess.Popen(cmd,  stdin=subprocess.PIPE,
                                      stdout=subprocess.PIPE,
                                       shell=True)
 
         stdout, stderr = proc.communicate()
-        if verbose:
+        if (not quiet) and verbose:
             print(c_cyan+stdout+c_end)
     except OSError as e:
         print(cmd)
