f07f42307c4bf3c83fc8449a18ef232ee8876af3
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-27 21:17:10 +0100

    better options for execute

--- toolbox/execute.py
+++ toolbox/execute.py
@@ -4,8 +4,8 @@ import subprocess
 c_end     = '\033[0m'
 c_cyan    = '\033[0;36m'
 
-def execute(cmd, verbose=True, quiet=False):
-    if not quiet:
+def execute(cmd, print_cmd=True, print_output=False):
+    if print_cmd:
         print(cmd)
     try:
         proc = subprocess.Popen(cmd,  stdin=subprocess.PIPE,
@@ -13,8 +13,9 @@ def execute(cmd, verbose=True, quiet=False):
                                       shell=True)
 
         stdout, stderr = proc.communicate()
-        if (not quiet) and verbose:
+        if print_output:
             print(c_cyan+stdout+c_end)
+        return stdout
     except OSError as e:
         print(cmd)
         raise e
